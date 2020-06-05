165417099cdc4eac1134825dd293c2eef7f124c5
adamrempala <rempalaad@gmail.com>
2020-06-05 11:43:27 +0200

    Do poprawki – ostatnie laby

--- zad4/tsfiles/app.ts
+++ zad4/tsfiles/app.ts
@@ -1,10 +1,42 @@
 import { MemeList } from './memelist'
+import csurf from 'csurf'
+import cookieParser from 'cookie-parser'
+// import session from 'express-session'
+// const niewiem = require('connect-sqlite3');
 
 export const lista = new MemeList();
 
 import express from 'express';
 
+//const sqliteStore = niewiem(session);
 const app = express();
+const secretlySecretValue = 'Mary had a little lamb';
+app.use(cookieParser(secretlySecretValue))
+
+const csrfProtection = csurf({cookie:true})
+
+// app.use(session({secret: secretlySecretValue,
+// cookie: { maxAge: 15*60*1000},
+// resave: false,
+// saveUninitialized: true/*,
+// ?8store: new sqliteStore() */}))
+
+
+app.use(express.urlencoded({
+
+  extended: true
+}));
+
+
+// app.use((req, res, next) => {
+//   if (!req.session?.views) {
+//     req.session.views = {};
+//   }
+//   req.session.views[req.path] = (req.session.views[req.path] || 0) + 1;
+//   next();
+// });
+
+
 
 app.set('view engine', 'pug');
 
@@ -14,22 +46,17 @@ app.get('/', (req, res) => {
 
 });
 
-app.get('/meme/:memeId', (req, res) => {
+app.get('/meme/:memeId', csrfProtection, (req, res) => {
 
   try {
     const meme = lista.getMeme(req.params.memeId);
-    res.render('prices', { meme })
+    res.render('prices', { meme, csrfToken: req.csrfToken() })
   } catch {
-    res.render('error', {message: '404 Not Found'})
+    res.render('error', {message: '404 Not Found', csrfToken: req.csrfToken()})
   }
 })
 
-app.use(express.urlencoded({
-
-  extended: true
-  }));
-
-  app.post('/meme/:memeId', (req, res) => {
+  app.post('/meme/:memeId', csrfProtection, (req, res) => {
 
     const meme = lista.getMeme(req.params.memeId);
 
@@ -37,12 +64,18 @@ app.use(express.urlencoded({
 
     meme.changePrice(price);
 
-    res.render('prices', { meme })
+    res.render('prices', { meme, csrfToken: req.csrfToken() })
 
   })
 
 
-  app.get('/*', (req, res) => {
-    res.render('error', {message: '404 Not Found'})
+  app.get('/*', csrfProtection, (req, res) => {
+    res.render('error', {message: '404 Not Found', csrfToken: req.csrfToken()})
   });
 module.exports = app;
+
+
+// app.post('/zaloguj', (req, res) => {
+//   // ... wyciągnij dane z formularza
+//   req.session.zalogowany = { username: wartosc };
+// })
